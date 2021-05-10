3f86b51cfd0e9dc6accf9b69e5b76029a0fa7ade
Kamil Wyszomierski <kamil.wyszomierski@gmail.com>
2021-05-10 21:16:03 +0200

    init

---
+++ YetAnotherMovieApp/Source/YetAnotherMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YetAnotherMovieAppApp.swift
+//  YetAnotherMovieApp
+//
+//  Created by Filo on 10/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct YetAnotherMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

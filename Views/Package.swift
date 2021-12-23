2fa1d4a8a3a2455452837e553de7c85acb62a02d
Kamil Wyszomierski <kamil.wyszomierski@gmail.com>
2021-12-23 21:57:18 +0100

    Bump minimal support version

--- Views/Package.swift
+++ Views/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Views",
-	platforms: [.iOS(.v14)],
+	platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "Views",
